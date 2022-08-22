548cf8ca325fb2ca4aa40fb9517f54722ac710cb
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-08-22 11:25:57 +0200

    Hero Image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import Banner from '../components/banner/banner';
 
@@ -22,6 +23,14 @@ export default function Home() {
           buttonText='View stores nearby'
           handleOnClick={handleOnBannerBtnClick}
         />
+        <div className={styles.heroImage}>
+          <Image
+            src='/static/hero-image.png'
+            width={700}
+            height={400}
+            alt='hero image'
+          />
+        </div>
       </main>
     </div>
   );
