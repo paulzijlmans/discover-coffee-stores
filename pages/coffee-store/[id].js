2f195d8496686fddba399f79afc401a0f7a9eb19
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-08-30 10:56:10 +0200

    Coffee Store Page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,48 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Head from 'next/head';
 
-function CoffeeStore() {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((store) => {
+        return store.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = coffeeStoresData.map((store) => {
+    return {
+      params: {
+        id: store.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+function CoffeeStore({ coffeeStore }) {
   const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading</div>;
+  }
   return (
     <div>
-      <h1>Coffee Store</h1>
-      <p>{router.query.id}</p>
+      <Head>
+        <title>{coffeeStore.name}</title>
+      </Head>
       <Link href='/'>Back to home</Link>
+      <p>{coffeeStore.address}</p>
+      <p>{coffeeStore.name}</p>
+      <p>{coffeeStore.neighbourhood}</p>
     </div>
   );
 }
