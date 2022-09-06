28aaf1e57b7ba521986542acbdd177ef4a4ba2ba
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-06 13:27:21 +0200

    Styling Update

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -53,7 +53,7 @@ function CoffeeStore({ coffeeStore: { name, imgUrl, address, neighborhood } }) {
       <div className={styles.container}>
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
-            <Link href='/'>Back to home</Link>
+            <Link href='/'>← Back to home</Link>
           </div>
           <div className={styles.nameWrapper}>
             <h1 className={styles.name}>{name}</h1>
